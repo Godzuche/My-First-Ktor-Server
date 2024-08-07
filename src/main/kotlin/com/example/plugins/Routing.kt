e1972c2bafe0a9ede41325411110f4562b67ddc4
Godzuche <godxuche@gmail.com>
2024-08-07 17:19:48 +0100

    finished the post requests, parameters and headers

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,11 @@
 package com.example.plugins
 
-import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     val users = mapOf(1 to "Ola", 2 to "Uche", 3 to "Victor")
@@ -15,8 +16,8 @@ fun Application.configureRouting() {
         }
 
         post("/page") {
-//            call.application.environment.log.info("\n ${call.receiveText()}")
-            try {
+            // Receive only text parameters
+            /*try {
                 // POST body parameters
                 val formParameters = call.receiveParameters()
                 val name = formParameters["name"]
@@ -27,9 +28,50 @@ fun Application.configureRouting() {
                     }
                 }
             } catch (e: ContentTransformationException) {
-                // Both parameters are missing
+                // Some parameters are missing
+                // returns 404 since no response
+            }*/
+
+            // Full access to all parameters including files using Multipart form data
+            try {
+                val parts = call.receiveMultipart().readAllParts()
+                parts.forEach { partData ->
+                    when (partData) {
+                        // If it is text parameter
+                        is PartData.FormItem -> {
+                            call.application.environment.log.info("${partData.name} : ${partData.value}")
+                        }
+                        // If it is file parameter
+                        is PartData.FileItem -> {
+                            val fileName = partData.originalFileName
+                            val fileBytes = partData.streamProvider().readBytes()
+                            val pathName = "avatars/$fileName"
+                            // Create a new file in the /avatars folder with the received name and content (bytes)
+                            File(pathName).writeBytes(fileBytes)
+                        }
+
+                        else -> Unit
+                    }
+                }
+            } catch (e: Exception){
+                call.application.environment.log.error(e.toString())
+                // Some parameters are missing
                 // returns 404 since no response
             }
+
+            // Getting Headers
+            call.request.headers.forEach { title, data -> call.application.environment.log.info("$title : ${data[0]}") }
+            // User-Agent (devices and browsers from which users access the site.)
+            call.application.environment.log.info(call.request.header("User-Agent").toString())
+            // Cookie Header
+            // A particular cookie
+            call.application.environment.log.info(call.request.cookies["Cookie_1"])
+            call.application.environment.log.info(call.request.headers["Cookie"])
+
+            // Getting Http method, host, and uri
+            call.application.environment.log.info(call.request.httpMethod.toString())
+            call.application.environment.log.info(call.request.host())
+            call.application.environment.log.info(call.request.uri)
         }
 
         route("/main") {
