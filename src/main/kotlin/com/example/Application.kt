598bb5ccbb70d508b70a072258639715d68e3f1e
Godzuche <godxuche@gmail.com>
2024-08-07 15:33:40 +0100

    refactor

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,17 +4,15 @@ import com.example.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.httpsredirect.*
 
 //Host 0.0.0.0 corresponds to the localhost. If you want to access the server only locally,
 // you can specify 127.0.0.1 or localhost.
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::mainModule)
         .start(wait = true)
 }
 
-// Todo: Consider renaming this to `mainModule`
-fun Application.module() {
+fun Application.mainModule() {
     installPlugins()
 }
--- src/main/kotlin/com/example/plugins/PluginModule.kt
+++ src/main/kotlin/com/example/plugins/PluginModule.kt
@@ -2,7 +2,6 @@ package com.example.plugins
 
 import io.ktor.server.application.*
 
-// Todo: Consider renaming this to `pluginsModule`
-fun Application.installPlugins(){
+fun Application.installPlugins() {
     configureRouting()
 }
